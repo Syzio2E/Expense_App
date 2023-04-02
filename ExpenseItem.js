6db3d77b968c2ab910f7e3836128b0fc5532975d
Shubham saurabh <87587179+Syzio2E@users.noreply.github.com>
2023-04-03 03:25:52 +0530

    Add files via upload

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,11 +1,7 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021,2,28);
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = '294'
-    const LocationOfExpenditure = 'london'
- 
+
   return (
     <div className="expense-item">
       <div>{props.date.toISOString()}</div>
